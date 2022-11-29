ab6073ce9746c059d0cb50faf44f72e2667bdd45
insung <yk832@naver.com>
2022-11-29 23:44:27 +0900

    session Interval setting

--- src/main/java/com/insung/core/common/config/SessionListenerConfig.java
+++ src/main/java/com/insung/core/common/config/SessionListenerConfig.java
@@ -1,2 +1,22 @@
-package com.insung.core.common.config;public class SessionListenerConfig {
+package com.insung.core.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@Slf4j
+@Configuration
+public class SessionListenerConfig implements HttpSessionListener {
+
+    private static final int SESSION_TIMEOUT = 60; // sec
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        se.getSession().setMaxInactiveInterval(SESSION_TIMEOUT);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        log.info("session Destroyed!!");
+    }
 }
