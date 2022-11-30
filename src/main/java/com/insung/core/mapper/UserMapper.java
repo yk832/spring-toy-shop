e6dbd7cf453b9b07c1f9650f669bbbcdc3d12041
insung <yk832@naver.com>
2022-11-30 23:38:05 +0900

    중복검사를 위한 db 요청 로직 추가

--- src/main/java/com/insung/core/mapper/UserMapper.java
+++ src/main/java/com/insung/core/mapper/UserMapper.java
@@ -4,10 +4,7 @@ import com.insung.core.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Mapper
 @Repository
@@ -15,4 +12,7 @@ public interface UserMapper {
     int join(Map<String,Object> userDto);
 
     Optional<UserDto> findUser(String userId);
+
+    Optional<String> validationUserID(Map<String, Object> paramMap);
+    Optional<String> validationUserEmail(Map<String, Object> paramMap);
 }
--- src/main/java/com/insung/core/service/UserServiceImpl.java
+++ src/main/java/com/insung/core/service/UserServiceImpl.java
@@ -8,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
     @Service
@@ -19,7 +23,7 @@ import java.util.Map;
 
         private final CommonMapper commonMapper;
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        @Override
+    @Override
     public void join(Map<String,Object> paramMap) throws Exception{
         String pwd = paramMap.get("pwd").toString();
 
@@ -29,13 +33,26 @@ import java.util.Map;
         pwdEncr = "{bcrypt}" + pwdEncr;
         paramMap.put("pwd", pwdEncr);
 
-        int result = userMapper.join(paramMap);
+        validationByUserInfo(paramMap);
 
-        log.info("insert success : " + result);
+//        int result = userMapper.join(paramMap);
+//
+//        log.info("insert success : " + result);
+//
+//        if (result > 0) {
+//            int chk = commonMapper.insertAuth(paramMap);
+//            log.info("auth check : " + chk);
+//        }
+    }
 
-        if (result > 0) {
-            int chk = commonMapper.insertAuth(paramMap);
-            log.info("auth check : " + chk);
-        }
+    private void validationByUserInfo(Map<String, Object> paramMap) {
+        Optional<String> userid = userMapper.validationUserID(paramMap);
+        Optional<String> userEmail = userMapper.validationUserEmail(paramMap);
+        System.out.println("userid = " + userid);
+        System.out.println("userEmail = " + userEmail);
+        // TODO 검증실패 익셉션 만들기..
+        //  id, email 한번에 검증 할 수 있는 로직으로 바꾸기
+        userid.orElseThrow(new UserValidationFailException());
+
     }
 }
